f74c3bfb82086958de44851a41bb856e59f8c76e
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-02-01 07:42:29 +0530

    Minor changes to db for template related changes.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -7,8 +7,8 @@
 SELECT @rolesadmin := `id` FROM cmg_core_role WHERE slug = 'super-admin';
 SELECT @roleadmin := `id` FROM cmg_core_role WHERE slug = 'admin';
 
-INSERT INTO `cmg_core_permission` (`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`description`,`icon`,`createdAt`,`modifiedAt`) VALUES 
-	(1,1,'FoxSlider','foxslider','system','The permission foxslider is to manage foxslider from admin.',NULL,'2014-10-11 14:22:54','2014-10-11 14:22:54');
+INSERT INTO `cmg_core_permission` (`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`icon`,`description`,`createdAt`,`modifiedAt`) VALUES 
+	(1,1,'FoxSlider','foxslider','system',NULL,'The permission foxslider is to manage foxslider from admin.','2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @permfxs := `id` FROM cmg_core_permission WHERE slug = 'foxslider';
 
