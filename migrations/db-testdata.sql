b1f280101cd1858c53abfcc2102fdc95fbfd3c63
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-05-02 20:08:04 +0530

    Updated roles and permissions data for hierarchy changes.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -7,13 +7,11 @@
 SELECT @rolesadmin := `id` FROM cmg_core_role WHERE slug = 'super-admin';
 SELECT @roleadmin := `id` FROM cmg_core_role WHERE slug = 'admin';
 
-INSERT INTO `cmg_core_permission` (`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`icon`,`description`,`lValue`,`rValue`,`createdAt`,`modifiedAt`) VALUES 
-	(1,1,'FoxSlider','foxslider','system',NULL,'The permission foxslider is to manage foxslider from admin.',1,2,'2014-10-11 14:22:54','2014-10-11 14:22:54');
+INSERT INTO `cmg_core_permission` (`createdBy`,`modifiedBy`,`name`,`slug`,`type`,`icon`,`description`,`createdAt`,`modifiedAt`) VALUES 
+	(1,1,'FoxSlider','foxslider','system',NULL,'The permission foxslider is to manage foxslider from admin.','2014-10-11 14:22:54','2014-10-11 14:22:54');
 
 SELECT @permfxs := `id` FROM cmg_core_permission WHERE slug = 'foxslider';
 
 INSERT INTO `cmg_core_role_permission` VALUES 
 	(@rolesadmin,@permfxs),
 	(@roleadmin,@permfxs);
-	
-UPDATE cmg_core_permission SET rootId=@permfxs where id=@permfxs;
